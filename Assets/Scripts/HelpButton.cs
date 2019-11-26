34a0e9d775dc05503ebc048d90b6167caee64bab
LR228874 <LR228874@falmouth.ac.uk>
2019-11-26 17:09:22 +0000

    moved scripts for contract 4 into their own folder, added script for descending tone

--- Assets/Scripts/HelpButton.cs
+++ Assets/Scripts/HelpButton.cs
@@ -2,6 +2,14 @@
 using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+//using NaughtyAttributes;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class HelpButton : MonoBehaviour
 {
@@ -11,11 +19,83 @@ public class HelpButton : MonoBehaviour
     {
         Button btn = yourButton.GetComponent<Button>();
         btn.onClick.AddListener(TaskOnClick);
+
+        audioSource = GetComponent<AudioSource>();
+        outAudioClip = CreateToneAudioClipAscending(1500);
+        //PlayOutAudio();
     }
 
     void TaskOnClick()
     {
         Debug.Log("You have clicked the button!");
+        PlayOutAudio();
         SceneManager.LoadScene(1);
     }
-}
+
+
+
+        private AudioSource audioSource;
+        private AudioClip outAudioClip;
+        public int sampleDurationSecs = 5;//length, 5
+        public int tone = 1500;//tone, 1500
+
+    // Public APIs
+    public void PlayOutAudio()
+        {
+            audioSource.PlayOneShot(outAudioClip);
+        }
+
+
+        public void StopAudio()
+        {
+            audioSource.Stop();
+        }
+
+
+    private AudioClip CreateToneAudioClipAscending(int frequency)
+    {
+        int sampleRate = 44100;//sample rate per second
+        int sampleLength = sampleRate * sampleDurationSecs;//total samples 
+        float maxValue = 1f / 4f;//max values of the samples
+
+        var audioClip = AudioClip.Create("tone", sampleLength, 1, sampleRate, false);//the audio clip
+
+        float[] samples = new float[sampleLength];//array of floats for the samples
+        //loop to get all the samples
+        for (var i = 0; i < (int)sampleLength / 3; i++)//the audio clip is split into 3 parts to make a ascending sound
+        {//first third 
+            float s = Mathf.Sin(2.0f * Mathf.PI * frequency * ((float)i / (float)sampleRate));//sample from the sine wave
+            float v = s * maxValue;//lowering the max value of the sample
+            samples[i] = v;//adding the sample to the array
+        }
+
+        for (var i = (int)sampleLength / 3; i < (int)(sampleLength / 3) * 2; i++)//second third
+        {
+            float s = Mathf.Sin(2.0f * Mathf.PI * (frequency * 1.5f) * ((float)i / (float)sampleRate));//sample from the sine wave
+            float v = s * maxValue;//lowering the max value of the sample
+            samples[i] = v;//adding the sample to the array
+        }
+
+        for (var i = (int)(sampleLength / 3) * 2; i < (int)sampleLength; i++)//last third
+        {
+            float s = Mathf.Sin(2.0f * Mathf.PI * (frequency * 2.0f) * ((float)i / (float)sampleRate));//sample from the sine wave
+            float v = s * maxValue;//lowering the max value of the sample
+            samples[i] = v;//adding the sample to the array
+        }
+
+        audioClip.SetData(samples, 0);
+        return audioClip;//returns the audio clip
+    }
+
+
+#if UNITY_EDITOR
+    //[Button("Save Wav file")]
+    private void SaveWavFile()
+        {
+            string path = EditorUtility.SaveFilePanel("Where do you want the wav file to go?", "", "", "wav");
+            var audioClip = CreateToneAudioClipAscending(1500);
+            SaveWavUtil.Save(path, audioClip);
+        }
+#endif
+    }
+
--- README.md
+++ README.md
@@ -11,14 +11,14 @@ License:<br />
 We used the unlicense license, this is because we don't wish to profit off this work, but we also don't want to stop others from using our work for profit. We want other to be able to use and edit our scritps to help them understand more about creating audio in code.  
 
 Contract 1 by Lachezar Penchev:<br />
-Used 2D Sprites pack from unity Asset Store
-https://assetstore.unity.com/packages/essentials/asset-packs/2d-sprites-pack-73728
-Sound clips:
-https://freesound.org/people/DrMaysta/sounds/418509/
-https://freesound.org/people/LloydEvans09/sounds/187025/
-http://soundbible.com/627-Flapping-Wings.html
-Other links:
-https://www.youtube.com/watch?v=Yb3isH6j-iU
+Used 2D Sprites pack from unity Asset Store<br />
+https://assetstore.unity.com/packages/essentials/asset-packs/2d-sprites-pack-73728<br />
+Sound clips:<br />
+https://freesound.org/people/DrMaysta/sounds/418509/<br />
+https://freesound.org/people/LloydEvans09/sounds/187025/<br />
+http://soundbible.com/627-Flapping-Wings.html<br />
+Other links:<br />
+https://www.youtube.com/watch?v=Yb3isH6j-iU<br />
 
 Contract 4 by Luke Ryan:<br />
 To open my project open the unity project in the GitHub and select the "Opening" scence, then press the play button.
@@ -31,4 +31,5 @@ button on click(https://docs.unity3d.com/530/Documentation/ScriptReference/UI.Bu
 Close unity (https://answers.unity.com/questions/899037/applicationquit-not-working-1.html)<br />
 over button (https://answers.unity.com/questions/783279/46-ui-how-to-detect-mouse-over-on-button.html)<br />
 event system (https://docs.unity3d.com/2018.1/Documentation/ScriptReference/EventSystems.EventSystem.IsPointerOverGameObject.html)<br />
-rounding up (https://docs.unity3d.com/ScriptReference/Mathf.Round.html)
+rounding up (https://docs.unity3d.com/ScriptReference/Mathf.Round.html)<br />
+square waves (https://www.mathworks.com/help/matlab/math/square-wave-from-sine-waves.html)
